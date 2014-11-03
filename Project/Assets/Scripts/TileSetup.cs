9746ff664b5cb923c68e57c049f2db293ca38e18
hilman <hilmanbeyri@gmail.com>
2014-11-03 15:06:20 -0500

    - setting up scene

---
+++ Project/Assets/Scripts/TileSetup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSetup : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public Transform tile;
+
+	private float tileWidth;
+	private float tileHeight;
+
+	void Start()
+	{
+		ArrangeTile();
+	}
+
+	void ArrangeTile()
+	{
+		float tileWidth = tile.renderer.bounds.size.x;
+		float tileDepth = tile.renderer.bounds.size.z;
+		for(int i=0; i<height; i++)
+		{
+			for(int j=0; j<width; j++)
+			{
+				Transform t = Instantiate(tile, new Vector3(j*tileWidth, 0, i*tileDepth), Quaternion.identity) as Transform;
+			}
+		}
+	}
+
+}
