37a924e39d8ada3cb7d44e9a6bc3a490c2f23997
bysreg <hilman09121991@gmail.com>
2014-11-04 11:55:17 -0500

    + phidgets plugin + itween plugin + made GameController & Tile as prefabs - renamed TileSetup to TileController

---
+++ Project/Assets/Scripts/TileController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour {
+
+	public int boardWidth; // how many tiles for the board's width (x axis)
+	public int boardHeight; // how many tiles for the board's height (z axis)
+	public Transform tile;
+
+	private GameObject tilesLayer;
+	private GameObject[,] tilesObj;
+
+	void Awake()
+	{
+		tilesLayer = GameObject.Find("Tiles");
+	}
+
+	void Start()
+	{
+		InitializeTiles();
+	}
+
+	void InitializeTiles()
+	{
+		float tileWidth = tile.renderer.bounds.size.x;
+		float tileDepth = tile.renderer.bounds.size.z;
+		tilesObj = new GameObject[boardHeight, boardHeight];
+
+		for(int i=0; i<boardHeight; i++)
+		{
+			for(int j=0; j<boardWidth; j++)
+			{
+				Transform t = Instantiate(tile, new Vector3(j*tileWidth, 0, i*tileDepth), Quaternion.identity) as Transform;
+				t.name = tile.name + (i*boardHeight + j);
+				t.parent = tilesLayer.transform;
+				tilesObj[i, j] = t.gameObject;
+				print (i + " " + j);
+			}
+		}
+
+		//deactive place holder tile not in tiles layer
+		GameObject placeHolderTile = GameObject.Find("Tile");
+		placeHolderTile.SetActive(false);
+	}
+
+	void SetTileHeight(TileMovement tile, float targetHeight)
+	{
+		tile.SetTileHeight(targetHeight);
+	}
+}
---
+++ Project/Assets/Scripts/TileMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMovement : MonoBehaviour {
+
+	bool isMoving;
+	float targetHeight; // y axis
+	float moveTime = 2f;
+
+	public void SetTileHeight(float targetHeight)
+	{
+		if(isMoving)
+			return;
+
+		this.targetHeight = targetHeight;
+		isMoving = true;
+		iTween.MoveTo(this.gameObject, iTween.Hash("y", targetHeight, "easeType", "easeInOutExpo", "time", moveTime, "oncomplete", "OnMoveComplete", "oncompletetarget", this.gameObject));
+	}
+
+	public void OnMoveComplete(Object o)
+	{
+		isMoving = false;
+	}
+}
