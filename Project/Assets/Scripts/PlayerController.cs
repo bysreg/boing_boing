2c54aafe67335bebe990235cab8e08a2b3663bbc
Mahardiansyah Kartika <mahardiansyahkartika@gmail.com>
2014-11-05 22:42:12 -0500

    + modify player controller

--- Project/Assets/Scripts/PlayerController.cs
+++ Project/Assets/Scripts/PlayerController.cs
@@ -6,31 +6,26 @@ public class PlayerController : CharacterBaseController {
 	bool simulateWithKeyboard = false;
 
 	//ps move
+	MoveController moveController;
 	Vector3 initialOrientation;
+	public int psMoveIndex = 0;
 
 	// Use this for initialization
 	protected override void Start () {
 		base.Start ();
 
-		try
-		{
-			if(PSMoveInput.MoveControllers[0].Connected)
-			{
+		try {
+			if(PSMoveInput.IsConnected && PSMoveInput.MoveControllers[psMoveIndex].Connected) {
 				simulateWithKeyboard = false;
-			}
-			else
-			{
+			} else {
 				simulateWithKeyboard = true;
 			}
-		}
-		catch
-		{
+		} catch {
 			simulateWithKeyboard = true;
 		}
 
-		if (!simulateWithKeyboard)
-		{
-			//CalibratePsMove(); // todo : unfinished
+		if (!simulateWithKeyboard) {
+			moveController = PSMoveInput.MoveControllers[psMoveIndex];
 		}
 	}
 	
@@ -38,66 +33,17 @@ public class PlayerController : CharacterBaseController {
 	protected override void Update () {
 		base.Update ();
 
-		// steering
-		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-		transform.Rotate(0, rotation, 0);
-	}
+		// keboard
+		if (simulateWithKeyboard) {
+			// forward
+			if (Input.GetKeyDown(KeyCode.UpArrow)) {
+				MoveForward();
+			}
+			// steering
+			float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+			transform.Rotate(0, rotation, 0);
+		} else { // ps move
 
-	void CalibratePsMove()
-	{
-		MoveController moveController = null;
-		switch(index)
-		{
-		case 1:
-			moveController = PSMoveInput.MoveControllers[0];
-			break;
-		case 2:
-			moveController = PSMoveInput.MoveControllers[1];
-			break;
-		case 3:
-			moveController = PSMoveInput.MoveControllers[2];
-			break;
-		case 4:
-			moveController = PSMoveInput.MoveControllers[3];
-			break;
-		}
-
-
-//		if(PSMoveInput.IsConnected && PSMoveInput.MoveControllers[0].Connected) 
-//		{
-//			
-//			Vector3 gemPos, handlePos;
-//			MoveData moveData = PSMoveInput.MoveControllers[0].Data;
-//			float zOffset = 20;
-//			gemPos = moveData.Position;
-//			handlePos = moveData.HandlePosition;
-//		
-//			gemPos.z = -gemPos.z + zOffset;
-//			handlePos.z = -handlePos.z + zOffset;
-//			gem.transform.localPosition = gemPos;
-//			handle.transform.localPosition = handlePos;
-//			handle.transform.localRotation = Quaternion.LookRotation(gemPos - handlePos);
-//			handle.transform.Rotate(new Vector3(0,0,moveData.Orientation.z));
-//
-//			/* using quaternion rotation directly
-//			* the rotations on the x and y axes are inverted - i.e. left shows up as right, and right shows up as left. This code fixes this in case 
-//			* the object you are using is facing away from the screen. Comment out this code if you do want an inversion along these axes
-//			* 
-//			* Add by Karthik Krishnamurthy*/
-//
-//			temp = moveData.QOrientation;
-//			temp.x = -moveData.QOrientation.x;
-//			temp.y = -moveData.QOrientation.y;
-//			handle.transform.localRotation = temp;
-//		}	
-	}
-
-	protected override void FixedUpdate() {
-		base.FixedUpdate ();
-
-		// acceleration
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			MoveForward();
 		}
 	}
 }
