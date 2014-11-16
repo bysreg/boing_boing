91df735a5c8207c58236a246c9ca5283fb2db86e
Mahardiansyah Kartika <mahardiansyahkartika@gmail.com>
2014-11-16 17:46:18 -0500

    +

--- Project/Assets/Scripts/GameController.cs
+++ Project/Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
+	public static bool[] activePlayers = new bool[]{false,true,false,false};
+
 	public int activePlayersCount = 4;
 
 	Transform p1;
