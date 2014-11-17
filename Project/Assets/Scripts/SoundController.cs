5d7e366bb9017e0fd60c2e935515d65593c17ace
Fan You <youfan.noey@gmail.com>
2014-11-16 22:37:26 -0500

    change sound countdown

--- Project/Assets/Scripts/SoundController.cs
+++ Project/Assets/Scripts/SoundController.cs
@@ -11,6 +11,7 @@ public class SoundController : MonoBehaviour {
 		if(Application.loadedLevel == 1) {
 			//PlaySound ("Bgm_new",0.2f, true);
 			StartCoroutine(PlayWithDely(4f, "Bgm_new"));
+			StartCoroutine(PlayWithDely(1f,"countdown"));
 		}else if(Application.loadedLevel == 0) {
 			PlaySound ("select_bgm",0.5f, true);
 		}
@@ -61,6 +62,10 @@ public class SoundController : MonoBehaviour {
 
 	IEnumerator PlayWithDely(float delay_time, string name) {
 		yield return new WaitForSeconds(delay_time);
-		PlaySound(name, 0.2f,true);
+		if (name == "Bgm_new") {
+						PlaySound (name, 0.2f, true);
+				} else {
+						PlaySound (name, 1f, false);
+				}
 	}
 }
